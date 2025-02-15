6e2dab1d9b3425a966a56fdaaada9dbe0f1b3e50
Daeton <daeton.harris24@gmail.com>
2025-02-15 00:16:22 -0500

    adding sprite/animation change for different directions

--- levels/player/character_body_2d.gd
+++ levels/player/character_body_2d.gd
@@ -4,6 +4,15 @@ extends CharacterBody2D
 
 func get_input():
 	var input_direction = Input.get_vector("walk_left","walk_right","walk_up","walk_down")
+	if Input.is_action_pressed("walk_left"):
+	
+	if Input.is_action_pressed("walk_right"):
+	
+	if Input.is_action_pressed("walk_up"):
+	
+	if Input.is_action_pressed("walk_down"):
+	
+
 	velocity = input_direction * speed
 
 func _physics_process(delta):
