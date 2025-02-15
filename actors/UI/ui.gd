219f21247d6deb86a0343b4371fa05bdc9686725
Daeton <daeton.harris24@gmail.com>
2025-02-15 13:32:21 -0500

    Trying, and failing, to fix inventory lol

--- actors/UI/ui.gd
+++ actors/UI/ui.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 @export var spongeVar = 0
 @export var bagVar  = 0
-@export var broomVar  = 0
-@export var keyVar  = 0
+@export var broomVar  = 1
+@export var keyVar  = 1
+@export var isSpotOneUsed = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,25 +16,46 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if spongeVar == 0:
 		$Sponge.visible = false
+		isSpotOneUsed = true
 		
 	if broomVar == 0:
 		$Broom.visible = false
-		
+		isSpotOneUsed = true
+
 	if bagVar == 0:
 		$Bag.visible = false
-		
+		isSpotOneUsed = true
+
 	if keyVar == 0:
 		$Key.visible = false
-		
-	if spongeVar == 1:
-		$Sponge.visible = true
-		
-	if broomVar == 1:
-		$Broom.visible = true
-		
-	if bagVar == 1:
-		$Bag.visible = true
-		
-	if keyVar == 1:
-		$Key.visible = true
+		isSpotOneUsed = true
+
+	if isSpotOneUsed == false:
+		if spongeVar == 1:
+			$Sponge.visible = true
+			
+		if broomVar == 1:
+			$Broom.visible = true
+			
+		if bagVar == 1:
+			$Bag.visible = true
+			
+		if keyVar == 1:
+			$Key.visible = true
+	else:
+		if spongeVar == 1:
+			$Sponge.position = Vector2(240,240)
+			$Sponge.visible = true
+			
+		if broomVar == 1:
+			$Broom.position = Vector2(240,240)
+			$Broom.visible = true
+			
+		if bagVar == 1:
+			$Bag.position = Vector2(240,240)
+			$Bag.visible = true
+			
+		if keyVar == 1:
+			$Key.position = Vector2(240,240)
+			$Key.visible = true
 	pass
