5e5ee17ead548dc3aa225b07cc4e219fbce4e4d6
Daeton <daeton.harris24@gmail.com>
2025-02-15 22:31:44 -0500

    added canvasNode into test level for UI. Made text go away on click

--- actors/Dialouge/dialouge.gd
+++ actors/Dialouge/dialouge.gd
@@ -12,21 +12,28 @@ func _ready() -> void:
 func showText():
 	$dialougeOutline.visible = true
 	$Text.visible = true
+	$Button.visible = true
 	pass
 func hideText():
 	$dialougeOutline.visible = false
 	$Text.visible = false
+	$Button.visible = false
 	pass
 
 func dialouge(text):
 	#why does goDot not support function overloading lol
 	#Pass text here.
-	
 	#Will make visible no matter what. Must be made invisible again after
 	showText()
 	$Text.text = str(text)
 	pass
-	
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	hideText()
+	pass # Replace with function body.
