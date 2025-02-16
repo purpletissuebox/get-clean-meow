9575f984a5d305d8866d2da986b61dd51eb3f78b
purple tissue box <dirksenmatthew1@gmail.com>
2025-02-16 09:58:44 -0500

    add arm sprite and refactor code to let you win.

--- minigames/scrub/scrub.gd
+++ minigames/scrub/scrub.gd
@@ -2,11 +2,14 @@ extends Minigame
 
 const NUM_DIRTS:int = 20
 const BRUSH_RADIUS:int = 50
+const DMG_RATE:float = 20000.0
+const BUBBLE_THRESHOLD:float = 30.0
+const BUBBLE_SCENE:PackedScene = preload("res://minigames/scrub/bubble.tscn")
 
 var dirt_particles:Array[TextureRect]
 var old_pos:Vector2
-var grime_left:float
-@onready var sprite_2d: Sprite2D = $Sprite2D
+var bubble_timer:float
+@onready var hand_sprite: Sprite2D = $arm
 
 func _ready():
 	self.old_pos = self.get_local_mouse_position()
@@ -20,16 +23,29 @@ func _ready():
 		self.dirt_particles.append(dirt)
 
 func _process(delta:float):
+	if !game_won:
+		if remove_dirt(delta):
+			bubble_timer -= delta
+			if bubble_timer < 0:
+				bubble_timer = randf_range(0.05,0.2)
+				var bubble_instance = BUBBLE_SCENE.instantiate()
+				bubble_instance.position = self.old_pos + Vector2(randi_range(-100,100), randi_range(-100,100))
+				self.add_child(bubble_instance)
+				
+	self.hand_sprite.position = self.old_pos
+
+func remove_dirt(delta:float):
 	var new_pos = self.get_local_mouse_position()
 	var speed = (new_pos-old_pos).length()/delta
 	var score:float = 0
-	sprite_2d.position = new_pos
 	for dirt in self.dirt_particles:
 		if Geometry2D.get_closest_point_to_segment(dirt.position, new_pos, old_pos).distance_to(dirt.position) < BRUSH_RADIUS:
-			dirt.modulate.a = clampf(dirt.modulate.a - speed/20000, 0.0, 1.0)
+			dirt.modulate.a = clampf(dirt.modulate.a - speed/DMG_RATE, 0.0, 1.0)
 		score += dirt.modulate.a
 	self.old_pos = new_pos
 	
 	score /= NUM_DIRTS
 	if score <= 0.02:
-		self.exit.emit()
+		complete_minigame()
+	
+	return speed > BUBBLE_THRESHOLD
