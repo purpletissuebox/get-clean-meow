19efd0c3069ac7e89c1d117d04fa5826eb6290ca
purple tissue box <dirksenmatthew1@gmail.com>
2025-02-16 12:23:10 -0500

    play music when main menu loads

--- levels/title/mainmenu.gd
+++ levels/title/mainmenu.gd
@@ -1,10 +1,13 @@
 extends Control
 
+func _ready():
+	SoundDriver.play_bgm(SoundDriver.BGM.TITLE_THEME, 0)
+
 func start_game():
 	SignalBus.change_lvl.emit("res://levels/test_lvl/level.tscn", 1)
 
 func do_options():
-	pass
+	SignalBus.start_minigame.emit("res://minigames/scrub/game.tscn")
 
 func quit_game():
 	get_tree().quit()
