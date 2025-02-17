b46df2256f399f7323093fdf5bbab9632fc82583
purple tissue box <dirksenmatthew1@gmail.com>
2025-02-17 11:21:54 -0500

    add test actor. RESOLVE TODO: loadzone exposes static method to change levels for use in cutscenes, etc

--- actors/table/table.gd
+++ actors/table/table.gd
@@ -1,6 +1,11 @@
 extends Interactable
 
 func _interact(_args:Array):
-	var tableTalk = "I was spoken to!"
-	$"../CanvasLayer/Dialouge".dialouge(tableTalk)
-	$"../CanvasLayer/Ui".bagVar = 1
+	if self.state == 0:
+		SignalBus.start_minigame.emit("res://minigames/scrub/game.tscn")
+		await SignalBus.minigame_ended
+		self.state = 1
+	
+	#var tableTalk = "I was spoken to!"
+	#$"../CanvasLayer/Dialouge".dialouge(tableTalk)
+	#$"../CanvasLayer/Ui".bagVar = 1
--- levels/load_zone.gd
+++ levels/load_zone.gd
@@ -1,4 +1,4 @@
-extends Interactable
+class_name LoadZone extends Interactable
 
 @export var spawnID:int
 @export var sceneID:GlobalData.RoomID
@@ -7,7 +7,10 @@ func _ready():
 	self.obj_interaction = "Go to " + GlobalData.RoomID.keys()[self.sceneID].replace("_s_", "'s_").capitalize()
 
 func _interact(_args:Array):
-	GlobalContext.spawn_point_ID = self.spawnID
+	load_level(self.sceneID, self.spawnID)
+	
+static func load_level(level:GlobalData.RoomID, spawn:int):
+	GlobalContext.spawn_point_ID = spawn
 	GlobalContext.plot_progress[GlobalContext.level_ID] = GlobalContext.level_node.room_state
-	GlobalContext.level_ID = self.sceneID
-	SignalBus.change_lvl.emit(GlobalData.ROOM_PATHS[self.sceneID], 0.5)
+	GlobalContext.level_ID = level
+	SignalBus.change_lvl.emit(GlobalData.ROOM_PATHS[level], 0.5)
