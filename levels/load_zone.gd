3206c3f3d5638b89d93208684af2f1a7a7563f77
purple tissue box <dirksenmatthew1@gmail.com>
2025-02-17 10:26:01 -0500

    new level load protocol. TODO make this a function so levels load properly thru other means?

--- levels/load_zone.gd
+++ levels/load_zone.gd
@@ -4,8 +4,10 @@ extends Interactable
 @export var sceneID:GlobalData.RoomID
 
 func _ready():
-	self.obj_name = "Go to " + GlobalData.RoomID.keys()[self.sceneID].replace("_s_", "'s_").capitalize()
+	self.obj_interaction = "Go to " + GlobalData.RoomID.keys()[self.sceneID].replace("_s_", "'s_").capitalize()
 
 func _interact(_args:Array):
 	GlobalContext.spawn_point_ID = self.spawnID
+	GlobalContext.plot_progress[GlobalContext.level_ID] = GlobalContext.level_node.room_state
+	GlobalContext.level_ID = self.sceneID
 	SignalBus.change_lvl.emit(GlobalData.ROOM_PATHS[self.sceneID], 0.5)
