ee0a54a093680da86a10e55d795187eaa23ec62b
purple tissue box <dirksenmatthew1@gmail.com>
2025-02-17 10:22:38 -0500

    new class members for state. actor registers itself in the current level.

--- actors/interactable.gd
+++ actors/interactable.gd
@@ -1,6 +1,15 @@
 class_name Interactable extends Area2D
 
-@export var obj_name:String
+@export var obj_ID:String
+@export var obj_interaction:String
+var state:int
+static var num_interactables:int
+
+func _init():
+	if !self.obj_ID:
+		self.obj_ID = "actor_#" + str(num_interactables)
+		num_interactables += 1
+	self.state = GlobalContext.level_node.get_actor_state(self)
 
 func _interact(_args:Array):
 	pass
