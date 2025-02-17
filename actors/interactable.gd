0b7098664356452bc485841d727a38453e481680
purple tissue box <dirksenmatthew1@gmail.com>
2025-02-17 11:17:22 -0500

    add setter to enforce state dict. move init to enter tree to have access to exports.

--- actors/interactable.gd
+++ actors/interactable.gd
@@ -2,13 +2,13 @@ class_name Interactable extends Area2D
 
 @export var obj_ID:String
 @export var obj_interaction:String
-var state:int
+var state:int:
+	set(v):
+		GlobalContext.level_node.room_state[self.obj_ID] = v
+		state = v
 static var num_interactables:int
 
-func _init():
-	if !self.obj_ID:
-		self.obj_ID = "actor_#" + str(num_interactables)
-		num_interactables += 1
+func _enter_tree():
 	self.state = GlobalContext.level_node.get_actor_state(self)
 
 func _interact(_args:Array):
