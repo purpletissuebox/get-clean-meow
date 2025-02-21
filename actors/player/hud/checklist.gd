4037e73fd40a674b4c3def812da547e8a181da4b
purple tissue box <dirksenmatthew1@gmail.com>
2025-02-21 18:34:23 -0500

    checklist scene skeleton. it raises when moused over

---
+++ actors/player/hud/checklist.gd
@@ -0,0 +1,25 @@
+extends Control
+
+const TRAVEL_TIME:float = 0.75
+const ANCHOR_DIFF:float = 0.2
+
+@onready var texture_rect: TextureRect = $TextureRect
+
+var movementT:Tween = null
+
+func raise():
+	cancelTween()
+	movementT = create_tween().set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
+	movementT.tween_property(texture_rect, "anchor_bottom", 1, TRAVEL_TIME)
+	movementT.parallel().tween_property(texture_rect, "anchor_top", 0.8-ANCHOR_DIFF, TRAVEL_TIME)
+
+func lower():
+	cancelTween()
+	movementT = create_tween().set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
+	movementT.tween_property(texture_rect, "anchor_bottom", 1+ANCHOR_DIFF, TRAVEL_TIME)
+	movementT.parallel().tween_property(texture_rect, "anchor_top", 0.8, TRAVEL_TIME)
+
+func cancelTween():
+	if self.movementT:
+		self.movementT.kill()
+		self.movementT = null
