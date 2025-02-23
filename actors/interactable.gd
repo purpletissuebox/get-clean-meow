04d4572ea55249c1b9d4a4ca2eb8080d78bbb94d
Daeton <daeton.harris24@gmail.com>
2025-02-22 19:40:17 -0500

    fixed my fuckup with interactables

--- actors/interactable.gd
+++ actors/interactable.gd
@@ -2,23 +2,13 @@ class_name Interactable extends Area2D
 
 @export var obj_ID:String
 @export var obj_interaction:String
-
-
 var state:int:
 	set(v):
 		GlobalContext.level_node.room_state[self.obj_ID] = v
 		state = v
 
-#Need help with this, crashes game when you pass text
+func _enter_tree():
+	self.state = GlobalContext.level_node.get_actor_state(self)
+
 func _interact():
-	#THE LINE BELOW THIS
-	#SignalBus.trigger_conversation.emit(self.text.pick_random())
-	SignalBus.updateTrophy.emit()
-	$".".visible = false
-
-
-
-func _on_button_pressed() -> void:
-	SignalBus.trigger_conversation.emit(self.text.pick_random())
-	print("test")
-	pass # Replace with function body.
+	pass
