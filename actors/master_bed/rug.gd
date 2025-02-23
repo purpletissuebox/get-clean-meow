1f5a160a8c7805537d3b1178bd5044f28bbe1ec5
purple tissue box <dirksenmatthew1@gmail.com>
2025-02-23 13:05:22 -0500

    use new key actor.

--- actors/master_bed/rug.gd
+++ actors/master_bed/rug.gd
@@ -1,14 +1,18 @@
 extends Interactable
 
+@export var key:Interactable
+@export var discovery:Conversation
 @export var text:Array[Conversation]
 
 func _interact():
 	if GlobalContext.rugClean == false:
 		GlobalContext.rugClean = true
 		SignalBus.start_minigame.emit("res://minigames/scrubRug/game.tscn")
+		await SignalBus.minigame_ended
 		$cleanRug.visible = false
 		$"../../rolledRug".visible = true
-		$"../../HiddenKey".visible = true
+		key.appear()
+		SignalBus.trigger_conversation.emit(self.discovery)
 	else:
 		SignalBus.trigger_conversation.emit(self.text.pick_random())
 	pass
@@ -23,5 +27,4 @@ func _process(delta: float) -> void:
 	if GlobalContext.rugClean == true:
 		$cleanRug.visible = false
 		$"../../rolledRug".visible = true
-		$"../../HiddenKey".visible = true
 	pass
