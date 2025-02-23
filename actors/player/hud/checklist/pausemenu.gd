b1dd0b053ec0767110afb99eb0ffb0c77b24ae1f
purple tissue box <dirksenmatthew1@gmail.com>
2025-02-22 20:23:09 -0500

    add global for tasklist and completion status

--- actors/player/hud/checklist/pausemenu.gd
+++ actors/player/hud/checklist/pausemenu.gd
@@ -3,13 +3,26 @@ extends Control
 const SWEEP_TIME:float = 1.5
 
 @onready var clipboard_img: TextureRect = $TextureRect
+@onready var tasks:Array[HBoxContainer] = [
+	$TextureRect/VBoxContainer/HBoxContainer,
+	$TextureRect/VBoxContainer/HBoxContainer2,
+	$TextureRect/VBoxContainer/HBoxContainer3
+]
 
 func _ready():
 	self.hide()
 
+func update_tasks():
+	var i:int = 0
+	for taskname:String in GlobalContext.goals.keys():
+		tasks[i].get_child(0).button_pressed = GlobalContext.goals[taskname]
+		tasks[i].get_child(1).text = taskname
+		i += 1
+
 func show_clipboard():
+	update_tasks()
 	self.show()
-	get_tree().paused = true
+	GlobalContext.level_node.process_mode = Node.PROCESS_MODE_DISABLED
 	
 	var t = create_tween().set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_OUT)
 	t.tween_property(clipboard_img, "anchor_bottom", 1, SWEEP_TIME)
@@ -17,9 +30,9 @@ func show_clipboard():
 
 func hide_clipboard():
 	var t = create_tween().set_trans(Tween.TRANS_QUART).set_ease(Tween.EASE_IN)
-	t.tween_property(clipboard_img, "anchor_bottom", 2, SWEEP_TIME)
-	t.parallel().tween_property(clipboard_img, "anchor_top", 1, SWEEP_TIME)
+	t.tween_property(clipboard_img, "anchor_bottom", 2, SWEEP_TIME/2)
+	t.parallel().tween_property(clipboard_img, "anchor_top", 1, SWEEP_TIME/2)
 	
 	await t.finished
 	self.hide()
-	get_tree().paused = false
+	GlobalContext.level_node.process_mode = Node.PROCESS_MODE_INHERIT
--- drivers/global_ctxt.gd
+++ drivers/global_ctxt.gd
@@ -5,6 +5,8 @@ var level_ID:int
 var level_node:Level
 var spawn_point_ID:int = 0
 var plot_progress:Array[Dictionary]
+var goals:Dictionary
+var day:int
 
 func _ready():
 	for i in GlobalData.RoomID.NUM_ROOMS:
