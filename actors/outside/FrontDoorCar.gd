a5860388de7198506d4a1290e03de8c6f1222370
Daeton <daeton.harris24@gmail.com>
2025-02-22 21:34:57 -0500

    Im tired

--- actors/outside/FrontDoorCar.gd
+++ actors/outside/FrontDoorCar.gd
@@ -1,12 +1,22 @@
 extends Interactable
 
+@export var text : Conversation
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#checkToSeeIfYouHaveTrophy
-	#
+	
 	pass # Replace with function body.
 
-
+func breakWindow():
+	if 	GlobalContext.hasTrophy == true:
+		pass
+	else:
+		pass
+	
+func _interact():
+	print(self.text)
+	SignalBus.trigger_conversation.emit(self.text)
+	#breakWindow()
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
