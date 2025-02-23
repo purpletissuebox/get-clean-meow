2b70256e3e81f717ef48466f9e0b1d5d557ab820
purple tissue box <dirksenmatthew1@gmail.com>
2025-02-23 14:28:55 -0500

    art and no repeat text

--- actors/outside/intro_text.gd
+++ actors/outside/intro_text.gd
@@ -3,13 +3,13 @@ extends Interactable
 @export var txt:Conversation
 
 func _process(_delta):
-	if !state:
+	if state == 2:
+		byebye()
+	elif !state:
 		self.state = 1
 		SignalBus.trigger_conversation.emit(self.txt)
 		await SignalBus.conversation_ended
 		self.state = 2
-	elif state == 2:
-		byebye()
 
 func byebye():
 		self.get_parent().remove_child(self)
